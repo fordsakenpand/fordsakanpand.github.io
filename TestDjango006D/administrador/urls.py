be3526e7975c561ff258ce661017644d62f5179f
Sebas <sebastianqd16@gmail.com>
2021-07-11 23:08:24 -0400

    app de administrador

--- TestDjango006D/administrador/urls.py
+++ TestDjango006D/administrador/urls.py
@@ -1,5 +1,6 @@
+from django.urls import path 
 from .views import login
 
 urlpatterns = [
-    path ('login.html',Cambios,name="login")
+    path ('login.html',login,name="login")
 ]
--- TestDjango006D/administrador/views.py
+++ TestDjango006D/administrador/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
-def Index (request):
+def login (request):
     return render(request,'core/login.html')
