3437723ba8d89ecbac74772b89b6f19bd8c27d0a
diberchat <pobletejose20136@gmail.com>
2021-07-13 19:24:15 -0400

    models

--- TestDjango006D/core/models.py
+++ TestDjango006D/core/models.py
@@ -1,4 +1,15 @@
 from django.db import models
 
+ 
+# Create your models here.
+
+
+
+
+#class Administrador(models.Model):
+    #useradmin = models.CharField(max_length=50,verbose_name="Nombre de usuario")
+    #contra = models.CharField(primary_key=True,max_length=10,verbose_name="Contraseña")
+    #def __str__(self):
+        #return self.useradmin
 # Create your models here.
 
