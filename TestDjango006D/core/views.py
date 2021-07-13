0ce03915dbc7cf7ade6be7d2dd1c777c3ec478b0
Sebas <sebastianqd16@gmail.com>
2021-07-13 18:59:49 -0400

    Modificación de index

--- TestDjango006D/core/views.py
+++ TestDjango006D/core/views.py
@@ -1,16 +1,28 @@
 from django.shortcuts import render
 
 # Create your views here.
-def Index (request):
-    return render(request,'core/Index.html')
 def Cambios (request):
     return render(request,'core/Cambios.html')
 def Contacto (request):
     return render(request,'core/Contacto.html')
 def Electricidad (request):
     return render(request,'core/Electricidad.html')
+
+class titu:
+    def __init__(self,electro,caja,suspen):
+        self.electro=electro
+        self.caja=caja
+        self.suspen=suspen
+        super().__init__()
 def Index (request):
-    return render(request,'core/Index.html')
+
+    titulo=titu("Especialista en electricidad","Especialista en caja de cambios","Especialista en suspencion")
+    descrip=["Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, asperiores laboriosam eum incidunt consectetur, debitis cupiditate doloremque quibusdam fugiat nisi dolore facere distinctio accusantium quia, eligendi quas? In, corporis molestiae.",
+             "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptates dolore neque quaerat ratione obcaecati illo deleniti unde harum! Neque sint, qui ducimus officiis pariatur commodi veritatis? Deserunt non aut suscipit.",
+             "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit corrupti fuga adipisci sit voluptatum recusandae iste eos odio eligendi quia nobis explicabo vitae accusamus saepe, culpa sed magni quidem animi!"]
+    contexto={"titulo":titulo,"texto":descrip}
+
+    return render(request,'core/Index.html',contexto)
 def MecaCam (request):
     return render(request,'core/MecaCam.html')
 def MecaSus (request):
